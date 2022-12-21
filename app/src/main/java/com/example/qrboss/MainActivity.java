6e2ccba457a5052e3e88ae3a10ebc699ff455253
sourav <sourav.myewards@gmail.com>
2022-12-21 16:53:34 +0530

    copy to clipboard

--- app/src/main/java/com/example/qrboss/MainActivity.java
+++ app/src/main/java/com/example/qrboss/MainActivity.java
@@ -7,8 +7,12 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
--- app/src/main/java/com/example/qrboss/ResultPage.java
+++ app/src/main/java/com/example/qrboss/ResultPage.java
@@ -2,9 +2,15 @@ package com.example.qrboss;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ResultPage extends AppCompatActivity {
     TextView textView;
@@ -17,6 +23,20 @@ public class ResultPage extends AppCompatActivity {
         Intent intent = getIntent();
         String data = intent.getStringExtra("result");
         textView.setText(data);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("label", data);
+                clipboard.setPrimaryClip(clip);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(ResultPage.this, "Copied!", Toast.LENGTH_SHORT).show(); 
+                    }
+                },200);
+            }
+        });
 
     }
 }
