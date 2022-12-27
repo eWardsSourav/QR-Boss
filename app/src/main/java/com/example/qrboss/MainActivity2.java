0968b051e7a3a340e5cce376f6d3281a57e35b23
sourav <sourav.myewards@gmail.com>
2022-12-27 19:00:47 +0530

    new commit

--- app/src/main/java/com/example/qrboss/MainActivity2.java
+++ app/src/main/java/com/example/qrboss/MainActivity2.java
@@ -14,6 +14,7 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.zxing.WriterException;
 
@@ -29,41 +30,72 @@ public class MainActivity2 extends AppCompatActivity {
     private EditText edtValue;
     private ImageView qrImage;
     private String inputValue;
+    TextView black,beguni,purple,green;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         edtValue = findViewById(R.id.edittext);
-        btn = findViewById(R.id.btn);
+
         qrImage = findViewById(R.id.img);
-        btn.setOnClickListener(new View.OnClickListener() {
+        black = findViewById(R.id.black);
+        beguni = findViewById(R.id.beguni);
+        purple = findViewById(R.id.purple);
+        green = findViewById(R.id.green);
+        purple.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                inputValue = edtValue.getText().toString().trim();
-                if (inputValue.length() > 0) {
-                    WindowManager manager = (WindowManager) getSystemService(WINDOW_SERVICE);
-                    Display display = manager.getDefaultDisplay();
-                    Point point = new Point();
-                    display.getSize(point);
-                    int width = point.x;
-                    int height = point.y;
-                    int smallerDimension = Math.min(width, height);
-                    smallerDimension = smallerDimension * 3 / 4;
-
-                    qrgEncoder = new QRGEncoder(inputValue, null, QRGContents.Type.TEXT, smallerDimension);
-                    qrgEncoder.setColorBlack(Color.BLACK);
-                    qrgEncoder.setColorWhite(Color.WHITE);
-                    try {
-                        bitmap = qrgEncoder.getBitmap();
-                        qrImage.setImageBitmap(bitmap);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                } else {
-                    edtValue.setError("Error");
-                }
+              generateQr(R.color.purple_500);
             }
         });
+        green.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                generateQr(R.color.green);            }
+        });
+        black.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                generateQr(R.color.black);
+            }
+        });
+        beguni.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                generateQr(R.color.beguni);
+            }
+        });
+
+    }
+
+    private void generateQr(int color) {
+        inputValue = edtValue.getText().toString().trim();
+        if (inputValue.length() > 0) {
+            WindowManager manager = (WindowManager) getSystemService(WINDOW_SERVICE);
+            Display display = manager.getDefaultDisplay();
+            Point point = new Point();
+            display.getSize(point);
+            int width = point.x;
+            int height = point.y;
+            int smallerDimension = Math.min(width, height);
+            smallerDimension = smallerDimension * 3 / 4;
+
+
+
+
+            qrgEncoder = new QRGEncoder(inputValue, null, QRGContents.Type.TEXT, smallerDimension);
+            qrgEncoder.setColorBlack(color);
+            qrgEncoder.setColorWhite(Color.WHITE);
+            try {
+                bitmap = qrgEncoder.getBitmap(0);
+                qrImage.setImageBitmap(bitmap);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            edtValue.setError("Error");
+        }
+
     }
 }
